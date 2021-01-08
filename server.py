a0251971c7e562ea93eae8d0448ab23be60c743c
ShaneMazur <shane.e.mazur@gmail.com>
2021-01-08 05:20:22 +0000

    fixed various bugs

--- server.py
+++ server.py
@@ -16,44 +16,43 @@ curr_frame = None
 stream = VideoStream(usePiCamera=1).start()
 time.sleep(2.0)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/video_feed")
-def video_feed():
-    return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
-
 
 def update_frame():
+    global curr_frame, lock
     while True:
         frame = stream.read()
-        frame = imutils.resize(frame, width=400)
+        frame = imutils.resize(frame, width=800)
         with lock:
             curr_frame = frame.copy()
 
 
 def serve_feed():
-    # grab global references to the output frame and lock variables
+    global curr_frame, lock
     # loop over frames from the output stream
     while True:
         # wait until the lock is acquired
         with lock:
             # check if the output frame is available, otherwise skip
             # the iteration of the loop
-            if outputFrame is None:
+            if curr_frame is None:
                 continue
             # encode the frame in JPEG format
-            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+            (flag, encodedImage) = cv2.imencode(".jpg", curr_frame)
             # ensure the frame was successfully encoded
             if not flag:
                 continue
         # yield the output frame in the byte format
-        yield (
-            b"--frame\r\n"
-            b"Content-Type: image/jpeg\r\n\r\n" + bytearray(encodedImage) + b"\r\n"
-        )
+        yield b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + encodedImage.tobytes() + b"\r\n"
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(serve_feed(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == "__main__":
@@ -63,3 +62,6 @@ if __name__ == "__main__":
     t.start()
     # start the flask app
     app.run(host="0.0.0.0", port="8000", debug=True, threaded=True, use_reloader=False)
+
+
+stream.stop()
